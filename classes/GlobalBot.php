140d9e579400d9ce8ae84044f4589a2aa3b188ab
Роман Мещеряков <rom_ik@bk.ru>
2021-11-20 18:09:19 +0400

    чтобы не собирать по сто раз

---
+++ classes/GlobalBot.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TRADEBOT;
+
+class GlobalBot
+{
+    public static function getBotSettings($message)
+    {
+        $config = \TRADEBOT\Config::getInstance();
+        $url = $config->settingApp->bot_api . '/getBotSettings.php';
+        $post = [
+            'hash' => $config->settingApp->message_hash
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result, 1);
+    }
+}
